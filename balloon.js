12df4e4b9457235aee6a1af74ab1cb83f6582f3a
FusionEA <78285392+FusionEA@users.noreply.github.com>
2021-11-30 20:27:50 -0500

    Add files via upload

---
+++ balloon.js
@@ -0,0 +1,15 @@
+class Balloon extends BaseClass {
+    constructor(x,y){
+      super(x,y,50,50);
+      this.image = loadImage("sprites/HotAir.png");
+
+    }
+  
+    display() {
+      this.body.position.x = mouseX;
+      this.body.position.y = mouseY;
+  
+      super.display();
+    }
+  
+  }
